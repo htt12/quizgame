69b320c8cab8ec197053187899fe2edbbc8677a5
htt12 <harrisontruong2@gmail.com>
2018-03-21 12:49:47 -0700

    editing objects for questions

---
+++ main.js
@@ -0,0 +1,19 @@
+const questions = [
+    {
+        question:'Is the sky blue?',
+        answers: {
+            true: 'true',
+            false: 'false'
+        },
+        correctAnswer: 'true',
+    },
+    {
+        question:'Are the leaves blue?',
+        answers: {
+            true: 'true',
+            false: 'false'
+        },
+        correctAnswer: 'false',
+    }
+];
+
